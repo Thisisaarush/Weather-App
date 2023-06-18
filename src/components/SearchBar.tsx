b4e7e23bef93d26669716483909c698f32bfb111
Aarush Tanwar <tanwaraarush007@gmail.com>
2023-06-19 02:31:18 +0530

    fixed type error again

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -48,6 +48,10 @@ export const SearchBar = () => {
       `https://devapi.qweather.com/v7/weather/7d?location=${weather_data.coord.lon},${weather_data.coord.lat}&lang=en&unit=${currentUnit[0]}&key=${Q_API_KEY}`
     );
     const forecast_data = await forecast_response.json();
+    const uv: string =
+      forecast_data.daily[0] !== undefined
+        ? forecast_data?.daily[0]?.uvIndex
+        : "0";
     const forecast = forecast_data?.daily?.map(
       (day: { [key: string]: any }) => {
         return {
@@ -88,10 +92,7 @@ export const SearchBar = () => {
       city: currentCity,
       country: weather_data.sys?.country,
       date: day + ", " + date,
-      uv:
-        forecast_data?.daily?.length !== 0
-          ? forecast_data?.daily[0]?.uvIndex
-          : "0",
+      uv: uv,
       air: air_pollution?.list[0]?.main?.aqi,
       forecast: forecast?.slice(1),
     });
